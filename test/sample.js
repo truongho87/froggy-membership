2be8227e463e5732dbfee0bdc353ffae62406b5e
txuanho <truonghx87@outlook.com>
2015-05-30 11:24:57 +0700

    Initial

---
+++ test/sample.js
@@ -0,0 +1,17 @@
+
+
+var assert = require("assert");
+
+describe("Something", function(){
+
+	describe("The way it smells", function(){
+
+		it("is pugent", function(){
+
+			assert.ok(true);
+
+		});
+
+	});
+
+});
